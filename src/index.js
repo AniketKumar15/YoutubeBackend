b83601c14c79f6f323c38656c2d1e153f26fb5bb
Aniket Kumar <110549582+AniketKumar15@users.noreply.github.com>
2025-04-06 12:14:03 +0530

    Add routers and controllers part1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
+import { app } from "./app.js"; // Correct import with curly braces
 import connectDB from "./db/connection.js";
 import dotenv from "dotenv";
-import app from "./app.js"
+
 dotenv.config({
     path: "./env"
-})
-
+});
 
 connectDB()
     .then(() => {
         app.listen(process.env.PORT || 3000, () => {
-            console.log(`App is started on the port no. -> ${process.env.PORT}`)
-        })
+            console.log(`App is started on the port no. -> ${process.env.PORT}`);
+        });
     })
     .catch((error) => {
-        console.log(`Mongo Db connection is failed(index.js) -> ${error}`)
-    })
+        console.log(`Mongo DB connection failed (index.js) -> ${error}`);
+    });
---
+++ src/routes/user.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { registerUser } from "../controllers/user.controllers.js";
+
+const route = Router();
+
+route.route("/register").post(registerUser);
+
+export default route
